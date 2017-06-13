6a6b18e6dd593a3492ec5d3ce9c81ec649a2aa02
Miguel Flores Gaspar <xxfinalmikexx@gmail.com>
2017-06-13 11:35:55 -0500

    Agregada lógica para el login

--- app/src/main/java/com/example/xxfin/tesinarecommendationsystem/LoginActivity.java
+++ app/src/main/java/com/example/xxfin/tesinarecommendationsystem/LoginActivity.java
@@ -34,16 +34,141 @@ import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
+/*Firebase imports*/
+import com.google.firebase.auth.*;
+
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
+    /*Firebase*/
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    
+    /*Buttons and Views*/
+    private EditText mEmailField;
+    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        //Initialize authentication
+        mAuth = FirebaseAuth.getInstance();
+        
+        // Views
+        mEmailField = (EditText) findViewById(R.id.field_email);
+        mPasswordField = (EditText) findViewById(R.id.field_password);
+        
+        
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                // [START_EXCLUDE]
+                updateUI(user);
+                // [END_EXCLUDE]
+            }
+        };
+    }
+    
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("username", user.getEmail());
+            intent.putExtra("userId", user.getUid());
 
+            this.startActivity(intent);
+        } else {
+            findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
+            findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+        }
+    }
+    
+    private void createAccount(View v) {
+            String email = mEmailField.getText().toString();
+            String password = mPasswordField.getText().toString();
+            
+            if(!validateForm()) {
+                return;   
+            } else {
+                    // [START create_user_with_email]
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, R.string.auth_failed,
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+            }
+    }
+    
+    private void signIn(View v) {
+        String email = eMailField.getText().toString();
+        String password = ePasswordField.getText().toString();
+        
+        if (!validateForm()) {
+            return;
+        } else {
+                // [START sign_in_with_email]
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, R.string.auth_failed,
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+        }
+    }
+    
+    public void signOut(View v) {
+        mAuth.signOut();
+        updateUI(null);
+    }
+    
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }
